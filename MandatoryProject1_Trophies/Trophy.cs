fed89597468bc39327ba691e9e500f22391a583e
yue001 <yue001@edu.zealand.dk>
2024-09-10 14:04:32 +0200

    Add project files.

---
+++ MandatoryProject1_Trophies/Trophy.cs
@@ -0,0 +1,50 @@
+﻿namespace MandatoryProject1_Trophies
+{
+    public class Trophy
+    {
+        // Properties
+        private int Id {  get; set; }
+        public string Competition { get; set; } = "Athletics";
+        public int Year { get; set; }
+
+        /**
+         * Validators
+         **/
+        // Validate Competition
+        public void validateCompetition()
+        {
+            if (Competition == null)
+            {
+                throw new ArgumentNullException("Competition cannot be null.");
+            }
+
+            if(Competition.Length < 3)
+            {
+                throw new ArgumentOutOfRangeException("Competition must be at least 3 characters long.");
+            }
+        }
+
+        // Validate Year
+        public void validateYear()
+        {
+            if(Year < 1970)
+            {
+                throw new ArgumentOutOfRangeException("Year cannot be below 1970");
+            }
+
+            if (Year > 2024)
+            {
+                throw new ArgumentOutOfRangeException("Year cannot exceed 2024");
+            }
+        }
+
+        // ToString Method
+        public override string ToString()
+        {
+            return Id + " " + Competition + " " + Year;
+        }
+
+
+
+    }
+}
---
+++ MandatoryProject1_TrophiesTests/TrophyTests.cs
@@ -0,0 +1,43 @@
+﻿namespace MandatoryProject1_Trophies.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        private Trophy Trophy = new Trophy();
+
+        [TestMethod()]
+        public void StringCompetitionIsNull_ThrowException()
+        {
+            Trophy.Competition = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => Trophy.validateCompetition());
+        }
+       
+
+        [TestMethod()]
+        public void StringCompetitionLenghtIsOutOfRange_ThrowException()
+        {
+            Trophy.Competition = "At";
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Trophy.validateCompetition());
+        }
+
+       
+        [TestMethod()]
+        public void IntYearIsOutOfUpperRange_ThrowException()
+        {
+            Trophy.Year = 2025;
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Trophy.validateYear());
+        }
+        
+
+        [TestMethod()]
+        public void IntYearIsOutOfLowerRange_ThrowException()
+        {
+            Trophy.Year = 1969;
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Trophy.validateYear());
+        }        
+    }
+}
